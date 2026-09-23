0d644686de71fa80a0237b30b45bb6b4215e9677
EmmanuelDania-Dev <daniaemma05@gmail.com>
2026-09-23 06:07:42 -0500

    integrated flash messages to for user notifications and redirects
    Added flash messages for various user actions to improve feedback and user experience and also added a decent redirect experience after registration

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for, flash
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -41,7 +41,8 @@ def init_db():
 @app.route("/")
 def home():
     if "user_id" not in session:
-        return redirect("/login")
+        flash("Log in First!!", "warning")
+        return redirect(url_for('login'))
 
     conn = get_db()
     tasks = conn.execute(
@@ -55,27 +56,31 @@ def home():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        username = request.form["username"].strip()
-        password = request.form["password"]
+        username = request.form.get("username").strip()
+        password = request.form.get("password")
 
         if not username or not password:
-            return "Username and password are required."
+            flash("Username and password are required.", "warning")
+            return redirect(url_for('register'))
 
         hashed_password = generate_password_hash(password)
 
         try:
             conn = get_db()
-            conn.execute(
+            cursor = conn.execute(
                 "INSERT INTO users (username, password) VALUES (?, ?)",
                 (username, hashed_password)
             )
             conn.commit()
             conn.close()
 
-            return redirect("/login")
+            user_id = cursor.lastrowid
+            session["user_id"] = user_id
+
+            return redirect(url_for('home'))
 
         except sqlite3.IntegrityError:
-            return "Username already exists."
+            flash("Username already exists.", "danger")
 
     return render_template("register.html")
 
@@ -83,8 +88,8 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+        username = request.form.get("username")
+        password = request.form.get("password")
 
         conn = get_db()
         user = conn.execute(
@@ -96,9 +101,10 @@ def login():
         if user and check_password_hash(user["password"], password):
             session["user_id"] = user["id"]
             session["username"] = user["username"]
-            return redirect("/")
+            return redirect(url_for('home'))
 
-        return "Invalid username or password."
+        flash("Invalid username or password.", "danger")
+        return redirect(url_for('login'))
 
     return render_template("login.html")
 
@@ -106,16 +112,17 @@ def login():
 @app.route("/logout")
 def logout():
     session.clear()
-    return redirect("/login")
+    return redirect(url_for('login'))
 
 
 @app.route("/add", methods=["POST"])
 def add():
     if "user_id" not in session:
-        return redirect("/login")
+        flash("Please login first", "warning")
+        return redirect(url_for('login'))
 
-    title = request.form["title"]
-    description = request.form["description"]
+    title = request.form.get("username")
+    description = request.form.get("description")
 
     conn = get_db()
     conn.execute(
@@ -125,17 +132,18 @@ def add():
     conn.commit()
     conn.close()
 
-    return redirect("/")
+    return redirect(url_for('home'))
 
 
 @app.route("/update/<int:id>", methods=["POST"])
 def update(id):
     if "user_id" not in session:
-        return redirect("/login")
+        flash("You must be logged in!", "warning")
+        return redirect(url_for('login'))
 
-    title = request.form["title"]
-    description = request.form["description"]
-    status = request.form["status"]
+    title = request.form.get("title")
+    description = request.form.get("description")
+    status = request.form.get("status")
 
     conn = get_db()
     conn.execute(
@@ -145,13 +153,14 @@ def update(id):
     conn.commit()
     conn.close()
 
-    return redirect("/")
+    return redirect(url_for('home'))
 
 
 @app.route("/delete/<int:id>", methods=["POST"])
 def delete(id):
     if "user_id" not in session:
-        return redirect("/login")
+        flash("Please log in", "warning")
+        return redirect(url_for('login'))
 
     conn = get_db()
     conn.execute(
@@ -161,7 +170,7 @@ def delete(id):
     conn.commit()
     conn.close()
 
-    return redirect("/")
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
